75898bc6931fb84c5a4b99336cb754829f625b77
Nick <studyaga@mail.ru>
2021-05-06 20:42:58 +0300

    -fix header link

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <nav className='blue'>
             <div className="nav-wrapper">
-                <a href="/" className="brand-logo">REACT SHOP</a>
+                <a href="/shop" className="brand-logo">REACT SHOP</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><a href="https://github.com/the-lensky/shop"  rel="noreferrer" target="_blank">Repo</a></li>
                 </ul>
